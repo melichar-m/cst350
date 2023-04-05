8e5bf4be144443612cc981bbcc7adfcfeddd7cc6
melichar-m <michael.melichar2@gmail.com>
2023-04-05 09:01:50 -0700

    pushing working login with validation

--- Milestone-CST350/Milestone-CST350/Controllers/LoginController.cs
+++ Milestone-CST350/Milestone-CST350/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Milestone_CST350.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,10 +24,26 @@ namespace Milestone_CST350.Controllers
                 return View(model);
             }
 
-            // Perform authentication logic here
+            // Check if the user exists in the database
+            using (var connection = new MySqlConnection($"SERVER=127.0.0.1;DATABASE=cst350_milestone;UID=root;PASSWORD=root;"))
+            {
+                connection.Open();
+                using (var sqlStatement = new MySqlCommand("SELECT id FROM users WHERE email = @Email AND password = @Password", connection))
+                {
+                    sqlStatement.Parameters.AddWithValue("@Email", model.Email);
+                    sqlStatement.Parameters.AddWithValue("@Password", model.Password);
+                    var result = sqlStatement.ExecuteScalar();
+                    if (result == null)
+                    {
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                        return View(model);
+                    }
+                }
+            }
 
             return RedirectToAction("Index", "Home");
         }
+
     }
 
 }
